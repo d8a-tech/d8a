d22ff8e53b600b90c10cc8157cd96bb00a51b2ca
Archibald Brightbottle <archibald.brightbottle@gmail.com>
2025-11-12 00:03:00 +0100

    Added missing docs for totals and unique columns

--- pkg/protocol/ga4/columns_session.go
+++ pkg/protocol/ga4/columns_session.go
@@ -1,6 +1,8 @@
 package ga4
 
 import (
+	"fmt"
+
 	"github.com/apache/arrow-go/v18/arrow"
 	"github.com/d8a-tech/d8a/pkg/columns"
 	"github.com/d8a-tech/d8a/pkg/schema"
@@ -352,6 +354,10 @@ var sessionTotalPageViewsColumn = columns.TotalEventsOfGivenNameColumn(
 	columns.CoreInterfaces.SessionTotalPageViews.ID,
 	columns.CoreInterfaces.SessionTotalPageViews.Field,
 	[]string{PageViewEventType},
+	columns.WithSessionColumnDocs(
+		"Session Total Page Views",
+		fmt.Sprintf("The total number of page views (event name: %s) in the session.", PageViewEventType), // nolint:lll // it's a description
+	),
 )
 
 var sessionUniquePageViewsColumn = columns.UniqueEventsOfGivenNameColumn(
@@ -367,18 +373,30 @@ var sessionUniquePageViewsColumn = columns.UniqueEventsOfGivenNameColumn(
 			GreaterOrEqualTo: "1.0.0",
 		},
 	),
+	columns.WithSessionColumnDocs(
+		"Session Unique Page Views",
+		fmt.Sprintf("The unique number of page views (event name: %s) in the session. Deduplicated by %s.", PageViewEventType, columns.CoreInterfaces.EventPageLocation.Field.Name), // nolint:lll // it's a description
+	),
 )
 
 var sessionTotalScrollsColumn = columns.TotalEventsOfGivenNameColumn(
 	columns.CoreInterfaces.SessionTotalScrolls.ID,
 	columns.CoreInterfaces.SessionTotalScrolls.Field,
 	[]string{ScrollEventType},
+	columns.WithSessionColumnDocs(
+		"Session Total Scrolls",
+		fmt.Sprintf("The total number of scrolls (event name: %s) in the session.", ScrollEventType), // nolint:lll // it's a description
+	),
 )
 
 var sessionTotalOutboundClicksColumn = columns.TotalEventsOfGivenNameColumn(
 	columns.CoreInterfaces.SessionTotalOutboundClicks.ID,
 	columns.CoreInterfaces.SessionTotalOutboundClicks.Field,
 	[]string{ClickEventType},
+	columns.WithSessionColumnDocs(
+		"Session Total Outbound Clicks",
+		fmt.Sprintf("The total number of outbound clicks (event name: %s) in the session.", ClickEventType), // nolint:lll // it's a description
+	),
 )
 
 var sessionUniqueOutboundClicksColumn = columns.UniqueEventsOfGivenNameColumn(
@@ -394,12 +412,20 @@ var sessionUniqueOutboundClicksColumn = columns.UniqueEventsOfGivenNameColumn(
 			GreaterOrEqualTo: "1.0.0",
 		},
 	),
+	columns.WithSessionColumnDocs(
+		"Session Unique Outbound Clicks",
+		fmt.Sprintf("The unique number of outbound clicks (event name: %s) in the session. Deduplicated by %s.", ClickEventType, ProtocolInterfaces.EventParamLinkURL.Field.Name), // nolint:lll // it's a description
+	),
 )
 
 var sessionTotalSiteSearchesColumn = columns.TotalEventsOfGivenNameColumn(
 	columns.CoreInterfaces.SessionTotalSiteSearches.ID,
 	columns.CoreInterfaces.SessionTotalSiteSearches.Field,
 	[]string{ViewSearchResultsEventType, SearchEventType},
+	columns.WithSessionColumnDocs(
+		"Session Total Site Searches",
+		fmt.Sprintf("The total number of site searches (event name: %s or %s) in the session.", ViewSearchResultsEventType, SearchEventType), // nolint:lll // it's a description
+	),
 )
 
 var sessionUniqueSiteSearchesColumn = columns.UniqueEventsOfGivenNameColumn(
@@ -409,12 +435,20 @@ var sessionUniqueSiteSearchesColumn = columns.UniqueEventsOfGivenNameColumn(
 	[]*arrow.Field{
 		ProtocolInterfaces.EventParamSearchTerm.Field,
 	},
+	columns.WithSessionColumnDocs(
+		"Session Unique Site Searches",
+		fmt.Sprintf("The unique number of site searches (event name: %s or %s) in the session. Deduplicated by %s.", ViewSearchResultsEventType, SearchEventType, ProtocolInterfaces.EventParamSearchTerm.Field.Name), // nolint:lll // it's a description
+	),
 )
 
 var sessionTotalFormInteractionsColumn = columns.TotalEventsOfGivenNameColumn(
 	columns.CoreInterfaces.SessionTotalFormInteractions.ID,
 	columns.CoreInterfaces.SessionTotalFormInteractions.Field,
 	[]string{FormSubmitEventType, FormStartEventType},
+	columns.WithSessionColumnDocs(
+		"Session Total Form Interactions",
+		fmt.Sprintf("The total number of form interactions (event name: %s or %s) in the session.", FormSubmitEventType, FormStartEventType), // nolint:lll // it's a description
+	),
 )
 
 var sessionUniqueFormInteractionsColumn = columns.UniqueEventsOfGivenNameColumn(
@@ -430,18 +464,30 @@ var sessionUniqueFormInteractionsColumn = columns.UniqueEventsOfGivenNameColumn(
 			GreaterOrEqualTo: "1.0.0",
 		},
 	),
+	columns.WithSessionColumnDocs(
+		"Session Unique Form Interactions",
+		fmt.Sprintf("The unique number of form interactions (event name: %s or %s) in the session. Deduplicated by %s.", FormSubmitEventType, FormStartEventType, ProtocolInterfaces.EventParamFormID.Field.Name), // nolint:lll // it's a description
+	),
 )
 
 var sessionTotalVideoEngagementsColumn = columns.TotalEventsOfGivenNameColumn(
 	columns.CoreInterfaces.SessionTotalVideoEngagements.ID,
 	columns.CoreInterfaces.SessionTotalVideoEngagements.Field,
 	[]string{VideoStartEventType, VideoCompleteEventType, VideoProgressEventType},
+	columns.WithSessionColumnDocs(
+		"Session Total Video Engagements",
+		fmt.Sprintf("The total number of video engagements (event name: %s, %s or %s) in the session.", VideoStartEventType, VideoCompleteEventType, VideoProgressEventType), // nolint:lll // it's a description
+	),
 )
 
 var sessionTotalFileDownloadsColumn = columns.TotalEventsOfGivenNameColumn(
 	columns.CoreInterfaces.SessionTotalFileDownloads.ID,
 	columns.CoreInterfaces.SessionTotalFileDownloads.Field,
 	[]string{FileDownloadEventType},
+	columns.WithSessionColumnDocs(
+		"Session Total File Downloads",
+		fmt.Sprintf("The total number of file downloads (event name: %s) in the session.", FileDownloadEventType), // nolint:lll // it's a description
+	),
 )
 
 var sessionUniqueFileDownloadsColumn = columns.UniqueEventsOfGivenNameColumn(
@@ -456,5 +502,9 @@ var sessionUniqueFileDownloadsColumn = columns.UniqueEventsOfGivenNameColumn(
 			Interface:        ProtocolInterfaces.EventParamLinkURL.ID,
 			GreaterOrEqualTo: "1.0.0",
 		},
+	),
+	columns.WithSessionColumnDocs(
+		"Session Unique File Downloads",
+		fmt.Sprintf("The unique number of file downloads (event name: %s) in the session. Deduplicated by %s.", FileDownloadEventType, ProtocolInterfaces.EventParamLinkURL.Field.Name), // nolint:lll // it's a description
 	),
 )
