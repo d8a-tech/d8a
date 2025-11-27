da6a74eb68f0d7020a11750f9e114b532f14709d
querypoppins <querypoppins@gmail.com>
2025-11-27 23:42:04 +0100

    New subdomain for docs (#153)

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -10,10 +10,10 @@ const config: Config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://d8a-tech.github.io/',
+  url: 'https://docs.d8a.tech/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: process.env.BASE_URL || '/d8a',
+  baseUrl: process.env.BASE_URL || '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
