dd7a0b238ceba4148706f74aad4cb96d27cd68f0
querypoppins <querypoppins@gmail.com>
2025-11-28 21:53:50 +0100

    SPA tracking for docs

---
+++ docs/plugins/spa-tracking.js
@@ -0,0 +1,24 @@
+function pushPageView() {
+    window.dataLayer = window.dataLayer || [];
+    window.dataLayer.push({
+        event: 'virtual_page_view',
+        page_title: document.title,
+        page_location: window.location.href,
+        page_referrer: document.referrer
+    });
+    console.log(document.location.href, document.title)
+}
+
+if (typeof window !== 'undefined') {
+    if (document.readyState === 'complete') {
+        pushPageView();
+    } else {
+        window.addEventListener('load', pushPageView);
+    }
+}
+
+export function onRouteDidUpdate({ location, previousLocation }) {
+    if (previousLocation) {
+        pushPageView();
+    }
+}
