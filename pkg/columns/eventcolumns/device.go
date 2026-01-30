0adc660e321e18913cad73c64ef13912b59be30f
Archibald Brightbottle <archibald.brightbottle@gmail.com>
2026-01-30 15:10:06 +0100

    Deduped some code in devices

--- pkg/columns/eventcolumns/device.go
+++ pkg/columns/eventcolumns/device.go
@@ -2,33 +2,45 @@
 package eventcolumns
 
 import (
+	"fmt"
+
+	"github.com/apache/arrow-go/v18/arrow"
+	"github.com/archbottle/dd2/pkg/detector"
 	"github.com/d8a-tech/d8a/pkg/columns"
 	"github.com/d8a-tech/d8a/pkg/schema"
-	"github.com/sirupsen/logrus"
 )
 
+func deviceColumn(
+	id schema.InterfaceID,
+	field *arrow.Field,
+	getValue func(*detector.FullInfo) (any, schema.D8AColumnWriteError),
+	opts ...columns.EventColumnOptions,
+) schema.EventColumn {
+	return columns.NewSimpleEventColumn(
+		id,
+		field,
+		func(e *schema.Event) (any, schema.D8AColumnWriteError) {
+			info, err := columns.DeviceFullInfo(e)
+			if err != nil || info == nil {
+				return nil, schema.NewBrokenEventError(
+					fmt.Sprintf("failed to get device full info: %v", err),
+				)
+			}
+			return getValue(info)
+		},
+		opts...,
+	)
+}
+
 // DeviceCategoryColumn is the column for the device category of an event
-var DeviceCategoryColumn = columns.NewSimpleEventColumn(
+var DeviceCategoryColumn = deviceColumn(
 	columns.CoreInterfaces.DeviceCategory.ID,
 	columns.CoreInterfaces.DeviceCategory.Field,
-	func(event *schema.Event) (any, schema.D8AColumnWriteError) {
-		info, err := columns.DeviceFullInfo(event)
-		if err != nil {
-			logrus.Warnf(
-				"DeviceCategoryColumn: %s: %v",
-				columns.CoreInterfaces.DeviceCategory.ID,
-				err,
-			)
+	func(info *detector.FullInfo) (any, schema.D8AColumnWriteError) {
+		if info.Device == nil || info.Device.Type == "" {
 			return nil, nil // nolint:nilnil // nil is valid
 		}
-		if info == nil || info.Device == nil {
-			return nil, nil // nolint:nilnil // nil is valid
-		}
-		deviceType := info.Device.Type
-		if deviceType == "" {
-			return nil, nil // nolint:nilnil // nil is valid
-		}
-		return deviceType, nil
+		return info.Device.Type, nil
 	},
 	columns.WithEventColumnDocs(
 		"Device Category",
@@ -37,27 +49,14 @@ var DeviceCategoryColumn = columns.NewSimpleEventColumn(
 )
 
 // DeviceMobileBrandNameColumn is the column for the device mobile brand name of an event
-var DeviceMobileBrandNameColumn = columns.NewSimpleEventColumn(
+var DeviceMobileBrandNameColumn = deviceColumn(
 	columns.CoreInterfaces.DeviceMobileBrandName.ID,
 	columns.CoreInterfaces.DeviceMobileBrandName.Field,
-	func(event *schema.Event) (any, schema.D8AColumnWriteError) {
-		info, err := columns.DeviceFullInfo(event)
-		if err != nil {
-			logrus.Warnf(
-				"DeviceMobileBrandNameColumn: %s: %v",
-				columns.CoreInterfaces.DeviceMobileBrandName.ID,
-				err,
-			)
+	func(info *detector.FullInfo) (any, schema.D8AColumnWriteError) {
+		if info.Device == nil || info.Device.Brand == "" {
 			return nil, nil // nolint:nilnil // nil is valid
 		}
-		if info == nil || info.Device == nil {
-			return nil, nil // nolint:nilnil // nil is valid
-		}
-		brand := info.Device.Brand
-		if brand == "" {
-			return nil, nil // nolint:nilnil // nil is valid
-		}
-		return brand, nil
+		return info.Device.Brand, nil
 	},
 	columns.WithEventColumnDocs(
 		"Device Brand (mobile)",
@@ -66,27 +65,14 @@ var DeviceMobileBrandNameColumn = columns.NewSimpleEventColumn(
 )
 
 // DeviceMobileModelNameColumn is the column for the device mobile model name of an event
-var DeviceMobileModelNameColumn = columns.NewSimpleEventColumn(
+var DeviceMobileModelNameColumn = deviceColumn(
 	columns.CoreInterfaces.DeviceMobileModelName.ID,
 	columns.CoreInterfaces.DeviceMobileModelName.Field,
-	func(event *schema.Event) (any, schema.D8AColumnWriteError) {
-		info, err := columns.DeviceFullInfo(event)
-		if err != nil {
-			logrus.Warnf(
-				"DeviceMobileModelNameColumn: %s: %v",
-				columns.CoreInterfaces.DeviceMobileModelName.ID,
-				err,
-			)
+	func(info *detector.FullInfo) (any, schema.D8AColumnWriteError) {
+		if info.Device == nil || info.Device.Model == "" {
 			return nil, nil // nolint:nilnil // nil is valid
 		}
-		if info == nil || info.Device == nil {
-			return nil, nil // nolint:nilnil // nil is valid
-		}
-		model := info.Device.Model
-		if model == "" {
-			return nil, nil // nolint:nilnil // nil is valid
-		}
-		return model, nil
+		return info.Device.Model, nil
 	},
 	columns.WithEventColumnDocs(
 		"Device Model (mobile)",
@@ -95,27 +81,14 @@ var DeviceMobileModelNameColumn = columns.NewSimpleEventColumn(
 )
 
 // DeviceOperatingSystemColumn is the column for the device operating system of an event
-var DeviceOperatingSystemColumn = columns.NewSimpleEventColumn(
+var DeviceOperatingSystemColumn = deviceColumn(
 	columns.CoreInterfaces.DeviceOperatingSystem.ID,
 	columns.CoreInterfaces.DeviceOperatingSystem.Field,
-	func(event *schema.Event) (any, schema.D8AColumnWriteError) {
-		info, err := columns.DeviceFullInfo(event)
-		if err != nil {
-			logrus.Warnf(
-				"DeviceOperatingSystemColumn: %s: %v",
-				columns.CoreInterfaces.DeviceOperatingSystem.ID,
-				err,
-			)
+	func(info *detector.FullInfo) (any, schema.D8AColumnWriteError) {
+		if info.OS == nil || info.OS.Name == "" {
 			return nil, nil // nolint:nilnil // nil is valid
 		}
-		if info == nil || info.OS == nil {
-			return nil, nil // nolint:nilnil // nil is valid
-		}
-		osName := info.OS.Name
-		if osName == "" {
-			return nil, nil // nolint:nilnil // nil is valid
-		}
-		return osName, nil
+		return info.OS.Name, nil
 	},
 	columns.WithEventColumnDocs(
 		"Operating System",
@@ -124,27 +97,14 @@ var DeviceOperatingSystemColumn = columns.NewSimpleEventColumn(
 )
 
 // DeviceOperatingSystemVersionColumn is the column for the device operating system version of an event
-var DeviceOperatingSystemVersionColumn = columns.NewSimpleEventColumn(
+var DeviceOperatingSystemVersionColumn = deviceColumn(
 	columns.CoreInterfaces.DeviceOperatingSystemVersion.ID,
 	columns.CoreInterfaces.DeviceOperatingSystemVersion.Field,
-	func(event *schema.Event) (any, schema.D8AColumnWriteError) {
-		info, err := columns.DeviceFullInfo(event)
-		if err != nil {
-			logrus.Warnf(
-				"DeviceOperatingSystemVersionColumn: %s: %v",
-				columns.CoreInterfaces.DeviceOperatingSystemVersion.ID,
-				err,
-			)
+	func(info *detector.FullInfo) (any, schema.D8AColumnWriteError) {
+		if info.OS == nil || info.OS.Version == "" {
 			return nil, nil // nolint:nilnil // nil is valid
 		}
-		if info == nil || info.OS == nil {
-			return nil, nil // nolint:nilnil // nil is valid
-		}
-		osVersion := info.OS.Version
-		if osVersion == "" {
-			return nil, nil // nolint:nilnil // nil is valid
-		}
-		return osVersion, nil
+		return info.OS.Version, nil
 	},
 	columns.WithEventColumnDocs(
 		"Operating System Version",
@@ -153,27 +113,14 @@ var DeviceOperatingSystemVersionColumn = columns.NewSimpleEventColumn(
 )
 
 // DeviceWebBrowserColumn is the column for the device web browser of an event
-var DeviceWebBrowserColumn = columns.NewSimpleEventColumn(
+var DeviceWebBrowserColumn = deviceColumn(
 	columns.CoreInterfaces.DeviceWebBrowser.ID,
 	columns.CoreInterfaces.DeviceWebBrowser.Field,
-	func(event *schema.Event) (any, schema.D8AColumnWriteError) {
-		info, err := columns.DeviceFullInfo(event)
-		if err != nil {
-			logrus.Warnf(
-				"DeviceWebBrowserColumn: %s: %v",
-				columns.CoreInterfaces.DeviceWebBrowser.ID,
-				err,
-			)
+	func(info *detector.FullInfo) (any, schema.D8AColumnWriteError) {
+		if info.Client == nil || info.Client.Name == "" {
 			return nil, nil // nolint:nilnil // nil is valid
 		}
-		if info == nil || info.Client == nil {
-			return nil, nil // nolint:nilnil // nil is valid
-		}
-		browserName := info.Client.Name
-		if browserName == "" {
-			return nil, nil // nolint:nilnil // nil is valid
-		}
-		return browserName, nil
+		return info.Client.Name, nil
 	},
 	columns.WithEventColumnDocs(
 		"Web Browser",
@@ -182,27 +129,14 @@ var DeviceWebBrowserColumn = columns.NewSimpleEventColumn(
 )
 
 // DeviceWebBrowserVersionColumn is the column for the device web browser version of an event
-var DeviceWebBrowserVersionColumn = columns.NewSimpleEventColumn(
+var DeviceWebBrowserVersionColumn = deviceColumn(
 	columns.CoreInterfaces.DeviceWebBrowserVersion.ID,
 	columns.CoreInterfaces.DeviceWebBrowserVersion.Field,
-	func(event *schema.Event) (any, schema.D8AColumnWriteError) {
-		info, err := columns.DeviceFullInfo(event)
-		if err != nil {
-			logrus.Warnf(
-				"DeviceWebBrowserVersionColumn: %s: %v",
-				columns.CoreInterfaces.DeviceWebBrowserVersion.ID,
-				err,
-			)
+	func(info *detector.FullInfo) (any, schema.D8AColumnWriteError) {
+		if info.Client == nil || info.Client.Version == "" {
 			return nil, nil // nolint:nilnil // nil is valid
 		}
-		if info == nil || info.Client == nil {
-			return nil, nil // nolint:nilnil // nil is valid
-		}
-		browserVersion := info.Client.Version
-		if browserVersion == "" {
-			return nil, nil // nolint:nilnil // nil is valid
-		}
-		return browserVersion, nil
+		return info.Client.Version, nil
 	},
 	columns.WithEventColumnDocs(
 		"Web Browser Version",
