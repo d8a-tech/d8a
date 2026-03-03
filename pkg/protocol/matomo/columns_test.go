2edffe2dfa1690efc372fb7f4b00b789df3dce3b
Archibald Brightbottle <archibald.brightbottle@gmail.com>
2026-03-04 00:29:43 +0100

    Matomo engagement cols

--- pkg/protocol/matomo/columns_test.go
+++ pkg/protocol/matomo/columns_test.go
@@ -1,24 +1,8 @@
 package matomo
 
 import (
-	"testing"
-
-	"github.com/d8a-tech/d8a/pkg/columns/columntests"
 	"github.com/d8a-tech/d8a/pkg/protocol"
-	"github.com/d8a-tech/d8a/pkg/warehouse"
-	"github.com/stretchr/testify/require"
 )
-
-func TestAllColumns(t *testing.T) {
-	columntests.ColumnTestCase(
-		t,
-		columntests.TestHits{columntests.TestHitOne(), columntests.TestHitTwo()},
-		func(t *testing.T, closeErr error, whd *warehouse.MockWarehouseDriver) {
-			require.NoError(t, closeErr)
-		},
-		NewMatomoProtocol(&staticPropertyIDExtractor{propertyID: "test_property_id"}),
-	)
-}
 
 type staticPropertyIDExtractor struct {
 	propertyID string
