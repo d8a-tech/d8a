001e306e8638ce8fbeca215e31c0c1578d138442
Archibald Brightbottle <archibald.brightbottle@gmail.com>
2026-03-04 15:12:53 +0100

    S

--- pkg/protocol/matomo/session_utm.go
+++ pkg/protocol/matomo/session_utm.go
@@ -50,3 +50,39 @@ var sessionUtmTermColumn = columns.NewSimpleSessionColumn(
 		return nil, nil
 	},
 )
+
+var sessionUtmIDColumn = columns.NewSimpleSessionColumn(
+	columns.CoreInterfaces.SessionUtmID.ID,
+	columns.CoreInterfaces.SessionUtmID.Field,
+	func(session *schema.Session) (any, schema.D8AColumnWriteError) {
+		// TODO(matomo): implement
+		return nil, nil
+	},
+)
+
+var sessionUtmSourcePlatformColumn = columns.NewSimpleSessionColumn(
+	columns.CoreInterfaces.SessionUtmSourcePlatform.ID,
+	columns.CoreInterfaces.SessionUtmSourcePlatform.Field,
+	func(session *schema.Session) (any, schema.D8AColumnWriteError) {
+		// TODO(matomo): implement
+		return nil, nil
+	},
+)
+
+var sessionUtmCreativeFormatColumn = columns.NewSimpleSessionColumn(
+	columns.CoreInterfaces.SessionUtmCreativeFormat.ID,
+	columns.CoreInterfaces.SessionUtmCreativeFormat.Field,
+	func(session *schema.Session) (any, schema.D8AColumnWriteError) {
+		// TODO(matomo): implement
+		return nil, nil
+	},
+)
+
+var sessionUtmMarketingTacticColumn = columns.NewSimpleSessionColumn(
+	columns.CoreInterfaces.SessionUtmMarketingTactic.ID,
+	columns.CoreInterfaces.SessionUtmMarketingTactic.Field,
+	func(session *schema.Session) (any, schema.D8AColumnWriteError) {
+		// TODO(matomo): implement
+		return nil, nil
+	},
+)
